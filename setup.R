0df442a087e9460ada4ccaaa9a6b35738d5050ff
rbkeeney1 <keeney.ryan@gmail.com>
2020-07-28 15:15:05 -0400

    Initial commit

--- setup.R
+++ setup.R
@@ -27,12 +27,14 @@ use_post("post/my-new-post") # creates new index.rmd w/ folder in posts folder
 
 # Create repo
 use_git()
+# save C:/Users/keene/Documents/R Projects/caladown
 
 # push repo out (i think)
 usethis::git_sitrep() # check auth key for github
 use_github()
-# for this to work the 1st time, I had to set the directory to this folder in bash: cd "C:\Users\keene\Documents\R Projects\caladown-site"
+# for this to work the 1st time, I had to set the directory to this folder in bash: cd "C:/Users/keene/Documents/R Projects/caladown"
 # then forced via: git push --set-upstream origin master
+# if locked... use: rm -f .git/index.lock
 
 #
 hugo_build(dest = "docs")
