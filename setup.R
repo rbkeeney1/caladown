f74fdc5229328bb6a47068690d8e346a3fdf224e
rbkeeney1 <keeney.ryan@gmail.com>
2020-07-28 18:43:58 -0400

    Initial commit

--- setup.R
+++ setup.R
@@ -18,23 +18,24 @@ library(git2r)
 
 
 # To launch the site:
-hugo_start()
+hugodown::hugo_start()
 
 
 # Create post
 use_post("post/my-new-post") # creates new index.rmd w/ folder in posts folder
 
-
 # Create repo
-use_git()
+usethis::use_git()
 # save C:/Users/keene/Documents/R Projects/caladown
 
 # push repo out (i think)
 usethis::git_sitrep() # check auth key for github
-use_github()
+usethis::use_github()
 # for this to work the 1st time, I had to set the directory to this folder in bash: cd "C:/Users/keene/Documents/R Projects/caladown"
 # then forced via: git push --set-upstream origin master
 # if locked... use: rm -f .git/index.lock
 
-#
-hugo_build(dest = "docs")
+
+
+#to create a new page, use above, then knit it, then use code below and commit + push updates out
+hugodown::hugo_build(dest = "docs")
